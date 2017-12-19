ad36972324af71dd141706b47a351e5c72f56872
Daryl Amodan <darylamodan@gmail.com>
2017-12-19 12:42:37 -0500

    adding the js

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,13 @@
-const ohHey = "Hello World";
 
-console.log(ohHeyyuppp);
+
+const inputs = document.querySelectorAll('.controls input');
+
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
+    
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate)); 
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate)); 
