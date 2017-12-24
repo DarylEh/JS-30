5c061f04a0a0a7b3b75c7edef2f3afe5921be264
Daryl Amodan <darylamodan@gmail.com>
2017-12-24 16:43:31 -0500

    finished exercise

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,14 @@
-const ohHey = "Hello World";
+const panels = document.querySelectorAll('.panel');
 
-console.log(ohHeyyuppp);
+function toggleOpen() {
+    this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active')
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
